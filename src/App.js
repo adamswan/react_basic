6ec86978911673f3d3eba530e8f91a15a7aeb3e4
adamswan <melyling@sina.com>
2024-10-16 11:20:02 +0800

    feat: 使用jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const age = 18;
+
+const getWife = () => '高圆圆'
+
+const obj = {
+  color: 'pink'
+}
+
 function App() {
-  return <div className="App">halo react</div>;
+  return (
+    <div className="App">
+      {/* 普通字符串 */}
+      {"hi"}
+
+      {/* 变量 */}
+      {age}
+
+      {/* 函数调用 */}
+      {getWife()}
+
+      {/* 调用内置方法 */}
+      {new Date().getFullYear()}
+
+      {/* 使用对象 */}
+      <div style={obj}>
+        文字颜色：{obj.color}
+      </div>
+    </div>
+  );
 }
 
 export default App;
