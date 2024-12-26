84efbfa7c07ef822b0062f7e0940ae8d52f79d55
adamswan <melyling@sina.com>
2024-12-26 22:59:44 +0800

    feat: 重启react的学习计划

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// react必要的两个核心包
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// 导入根组件
 import App from "./App";
 
+// 将根组件 App 挂载到 id 为 root 的 DOM 节点上
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
